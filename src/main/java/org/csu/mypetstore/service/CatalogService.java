76aa5451202859f30458797e21205762689ab2ec
s1mplezz <1303628536@qq.com>
2020-03-31 17:35:42 +0800

    thymeleaf 初始构造

--- src/main/java/org/csu/mypetstore/service/CatalogService.java
+++ src/main/java/org/csu/mypetstore/service/CatalogService.java
@@ -1,15 +1,28 @@
 package org.csu.mypetstore.service;
 
 import org.csu.mypetstore.domain.Category;
+import org.csu.mypetstore.domain.Product;
 import org.csu.mypetstore.persistence.CatergoryMapper;
+import org.csu.mypetstore.persistence.ProductMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CatalogService {
    @Autowired
     private CatergoryMapper catergoryMapper;
+   @Autowired
+   private ProductMapper productMapper;
+
    public Category getCategory(String categoryId){
        return catergoryMapper.getCategory(categoryId);
    }
+    public Product getProduct(String productId) {
+        return productMapper.getProduct(productId);
+    }
+    public List<Product> getProductListByCategory(String categoryId) {
+        return productMapper.getProductListByCategory(categoryId);
+    }
 }
